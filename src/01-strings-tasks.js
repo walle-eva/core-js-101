b18139d68637887e59b38ee9fa07a3a0b2ba01b6
walle-eva <walle-eva@users.noreply.github.com>
2020-05-12 14:58:51 +0700

    string

--- src/01-strings-tasks.js
+++ src/01-strings-tasks.js
@@ -34,7 +34,7 @@ function concatenateStrings(value1, value2) {
  *   ''      => 0
  */
 function getStringLength(value) {
-  return value.length; 
+  return value.length;
 }
 
 /**
@@ -51,7 +51,7 @@ function getStringLength(value) {
  *   'Chuck','Norris'  => 'Hello, Chuck Norris!'
  */
 function getStringFromTemplate(firstName, lastName) {
-  return `Hello, ${firstName} ${lastName}`;
+  return `Hello, ${firstName} ${lastName}!`;
 }
 
 /**
@@ -65,7 +65,10 @@ function getStringFromTemplate(firstName, lastName) {
  *   'Hello, Chuck Norris!' => 'Chuck Norris'
  */
 function extractNameFromTemplate(value) {
-  return value.split(' ')[1].slice(0, -1);
+  const arr = value.split(' ');
+  let str = `${arr[1]} ${arr[2]}`;
+  str = str.slice(0, -1);
+  return str;
 }
 
 
@@ -84,21 +87,6 @@ function getFirstChar(value) {
 }
 
 /**
- * Removes a leading and trailing whitespace characters from string.
- *
- * @param {string} value
- * @return {string}
- *
- * @example
- *   '  Abracadabra'    => 'Abracadabra'
- *   'cat'              => 'cat'
- *   '\tHello, World! ' => 'Hello, World!'
- */
-function removeLeadingAndTrailingWhitespaces(value) {
-  return;
-}
-
-/**
  * Returns a string that repeated the specified number of times.
  *
  * @param {string} value
@@ -111,8 +99,8 @@ function removeLeadingAndTrailingWhitespaces(value) {
  */
 function repeatString(value, count) {
   let result = '';
-  for(let i = 0; i < count; i++){
-	  result += value;
+  for (let i = 0; i < count; i += 1) {
+    result += value;
   }
   return result;
 }
@@ -130,13 +118,12 @@ function repeatString(value, count) {
  *   'ABABAB','BA' => 'ABAB'
  */
 function removeFirstOccurrences(str, value) {
-  let arr = str.split(' ');
-  for(let i = 0; i < arr.length; i++){
-	  if(arr[i] == value){
-		  arr.splice(i, 1)
-	  }
+  let newStr;
+  const length = str.indexOf(value);
+  if (str.indexOf(value) + 1) {
+    newStr = str.slice(0, length) + str.slice(length + value.length, str.length);
   }
-  return arr.join(' ');
+  return newStr;
 }
 
 /**
@@ -151,13 +138,13 @@ function removeFirstOccurrences(str, value) {
  *   '<a>' => 'a'
  */
 function unbracketTag(str) {
-  let arr = str.split('');
-  for(let i = 0; i < arr.length; i++){
-	  if((arr[i] == '<') || (arr[i] == '>')){
-		  arr.splice(i, 1)
-	  }
+  const arr = str.split('');
+  for (let i = 0; i < arr.length; i += 1) {
+    if ((arr[i] === '<') || (arr[i] === '>')) {
+      arr.splice(i, 1);
+    }
   }
-  return arr.join(' ');
+  return arr.join('');
 }
 
 
@@ -175,126 +162,9 @@ function convertToUpperCase(str) {
   return str.toUpperCase();
 }
 
-/**
- * Extracts e-mails from single string with e-mails list delimeted by semicolons
- *
- * @param {string} str
- * @return {array}
- *
- * @example
- *   'angus.young@gmail.com;brian.johnson@hotmail.com;bon.scott@yahoo.com'
- *   => [
- *      'angus.young@gmail.com',
- *      'brian.johnson@hotmail.com',
- *      'bon.scott@yahoo.com'
- *   ],
- *   'info@gmail.com' => ['info@gmail.com']
- */
 function extractEmails(str) {
   return str.split(';');
 }
-
-/**
- * Returns the string representation of rectangle with specified width and height
- * using pseudograhic chars
- *
- * @param {number} width
- * @param {number} height
- * @return {string}
- *
- * @example
- *
- *            '┌────┐\n'+
- *  (6,4) =>  '│    │\n'+
- *            '│    │\n'+
- *            '└────┘\n'
- *
- *  (2,2) =>  '┌┐\n'+
- *            '└┘\n'
- *
- *             '┌──────────┐\n'+
- *  (12,3) =>  '│          │\n'+
- *             '└──────────┘\n'
- *
- */
-function getRectangleString(width, height) {
-  let str = '';
-  for(let i = 0; i < width; i++){
-	  str += '_';
-  }
-  for(let i = 0; i < height; i++){
-	  str += '|';
-  }
-  return str;
-}
-
-
-/**
- * Encode specified string with ROT13 cipher
- * See details:  https://en.wikipedia.org/wiki/ROT13
- *
- * @param {string} str
- * @return {string}
- *
- * @example
- *
- *   'hello' => 'uryyb'
- *   'Why did the chicken cross the road?' => 'Jul qvq gur puvpxra pebff gur ebnq?'
- *   'Gb trg gb gur bgure fvqr!' => 'To get to the other side!'
- *   'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz'
- *    => 'NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm'
- *
- */
-function encodeToRot13(/* str */) {
-  throw new Error('Not implemented');
-}
-
-/**
- * Returns true if the value is string; otherwise false.
- * @param {string} value
- * @return {boolean}
- *
- * @example
- *   isString() => false
- *   isString(null) => false
- *   isString([]) => false
- *   isString({}) => false
- *   isString('test') => true
- *   isString(new String('test')) => true
- */
-function isString(/* value */) {
-  
-}
-
-
-/**
- * Returns playid card id.
- *
- * Playing cards inittial deck inclides the cards in the following order:
- *
- *  'A♣','2♣','3♣','4♣','5♣','6♣','7♣','8♣','9♣','10♣','J♣','Q♣','K♣',
- *  'A♦','2♦','3♦','4♦','5♦','6♦','7♦','8♦','9♦','10♦','J♦','Q♦','K♦',
- *  'A♥','2♥','3♥','4♥','5♥','6♥','7♥','8♥','9♥','10♥','J♥','Q♥','K♥',
- *  'A♠','2♠','3♠','4♠','5♠','6♠','7♠','8♠','9♠','10♠','J♠','Q♠','K♠'
- *
- * (see https://en.wikipedia.org/wiki/Standard_52-card_deck)
- * Function returns the zero-based index of specified card in the initial deck above.
- *
- * @param {string} value
- * @return {number}
- *
- * @example
- *   'A♣' => 0
- *   '2♣' => 1
- *   '3♣' => 2
- *     ...
- *   'Q♠' => 50
- *   'K♠' => 51
- */
-function getCardId(/* value */) {
-  
-}
-
 
 module.exports = {
   concatenateStrings,
@@ -302,14 +172,9 @@ module.exports = {
   getStringFromTemplate,
   extractNameFromTemplate,
   getFirstChar,
-  removeLeadingAndTrailingWhitespaces,
   repeatString,
   removeFirstOccurrences,
   unbracketTag,
   convertToUpperCase,
   extractEmails,
-  getRectangleString,
-  encodeToRot13,
-  isString,
-  getCardId,
 };
